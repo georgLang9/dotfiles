1a9c6e56ca6fbfb7e8ab4472c51041f44466b5ef
langgeorg61@gmail.com <langgeorg61@gmail.com>
2023-09-08 21:32:00 +0200

    removed unnecessary ;;

--- install.sh
+++ install.sh
@@ -68,30 +68,30 @@ install_all() {
 # Install zsh + oh-my-zsh + powerlevel10k
 install_zsh() {
 	echo "Installing rust..."
-  $FILE=/usr/bin/rustup
-  if test -f "$FILE"; then
-    rustup update
-  else 
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-    source "/home/$USER/.cargo/env"
-  fi
+	$FILE=/usr/bin/rustup
+	if test -f "$FILE"; then
+		rustup update
+	else
+		curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+		source "/home/$USER/.cargo/env"
+	fi
 
 	echo "Installing zsh, oh-my-zsh and powerlevel10k..."
-  install_packages zsh
+	install_packages zsh
 
-  $FILE=/home/$USER/.oh-my-zsh
-  if [ -d "$FILE" ]; then
-    echo "oh-my-zsh already installed..."
-  else
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-  fi
+	$FILE=/home/$USER/.oh-my-zsh
+	if [ -d "$FILE" ]; then
+		echo "oh-my-zsh already installed..."
+	else
+		sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	fi
 
-  $FILE=/home/$USER/.oh-my-zsh/custom/themes/powerlevel10k
-  if [ -d "$FILE" ]; then
-    echo "powerlevel10k already installed..."
-  else
-	  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-  fi
+	$FILE=/home/$USER/.oh-my-zsh/custom/themes/powerlevel10k
+	if [ -d "$FILE" ]; then
+		echo "powerlevel10k already installed..."
+	else
+		git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+	fi
 
 	cp ./.zshrc /home/$USER/
 }
@@ -100,43 +100,42 @@ install_zsh() {
 # Install wezterm
 install_wezterm() {
 	echo "Installing wezterm..."
-  install_packages wezterm
+	install_packages wezterm
 
-  cp ./.wezterm.lua /home/$USER/
+	cp ./.wezterm.lua /home/$USER/
 }
 
 #=====================================================
 # Install neovim
 install_neovim() {
 	echo "Installing neovim..."
-  install_packages neovim
-
+	install_packages neovim
 
 	rm -rf /home/$USER/.config/nvim/.git
-  install_packages lazygit
+	install_packages lazygit
 
 	# for telescope:
-  install_packages ripgrep fd
+	install_packages ripgrep fd
 
 	echo "Installing lazyvim..."
-  $FILE=/home/$USER/.config/nvim/lua/
-  if [ -d "$FILE" ]; then
-    echo "lazyvim already installed..."
-  else
-    # required
-    rm /home/$USER/.config/nvim{,.bak}
+	$FILE=/home/$USER/.config/nvim/lua/
+	if [ -d "$FILE" ]; then
+		echo "lazyvim already installed..."
+	else
+		# required
+		rm /home/$USER/.config/nvim{,.bak}
 
-    # optional but recommended
-    rm /home/$USER/.local/share/nvim{,.bak}
-    rm /home/$USER/.local/state/nvim{,.bak}
-    rm /home/$USER/.cache/nvim{,.bak}
+		# optional but recommended
+		rm /home/$USER/.local/share/nvim{,.bak}
+		rm /home/$USER/.local/state/nvim{,.bak}
+		rm /home/$USER/.cache/nvim{,.bak}
 
-    git clone https://github.com/LazyVim/starter /home/$USER/.config/nvim
+		git clone https://github.com/LazyVim/starter /home/$USER/.config/nvim
 
-    rm -rf ~/.config/nvim/.git
-  fi
+		rm -rf ~/.config/nvim/.git
+	fi
 
-  echo "updating config..."
+	echo "updating config..."
 	cp -r ./.config/nvim/* /home/$USER/
 }
 
@@ -144,24 +143,23 @@ install_neovim() {
 # doom emacs
 install_dEmacs() {
 	echo "Installing doom emacs"
-  install_packages emacs 
+	install_packages emacs
 
+	$FILE=/home/$USER/.config/emacs/bin/doom
+	if [ -f "$FILE" ]; then
+		echo "doom emacs already installed..."
+	else
+		git clone --depth 1 https://github.com/doomemacs/doomemacs /home/$USER/.config/emacs
+		$FILE install
+	fi
 
-  $FILE=/home/$USER/.config/emacs/bin/doom
-  if [ -f "$FILE" ]; then
-    echo "doom emacs already installed..."
-  else
-	  git clone --depth 1 https://github.com/doomemacs/doomemacs /home/$USER/.config/emacs
-    $FILE install
-  fi
-
-  $FILE=/home/$USER/.config/autostart/emacs.desktop
-  if [ -f "$FILE" ]; then
-    echo "autostart for emacs exists"
-  else
-	  mkdir /home/$USER/.config/autostart/
-	  cp ./emacs.desktop /home/$USER/.config/autostart/.
-  fi
+	$FILE=/home/$USER/.config/autostart/emacs.desktop
+	if [ -f "$FILE" ]; then
+		echo "autostart for emacs exists"
+	else
+		mkdir /home/$USER/.config/autostart/
+		cp ./emacs.desktop /home/$USER/.config/autostart/.
+	fi
 
 	doom sync
 }
@@ -170,13 +168,13 @@ install_dEmacs() {
 # hyprland
 install_hyprland() {
 	echo "Installing hyprland..."
-  if pacman -Qs hyprland-git >/dev/null; then
-    echo "hyprland-git is already installed..."
-  else
-    sudo -u $USER yay -S hyprland-git
-  fi
+	if pacman -Qs hyprland-git >/dev/null; then
+		echo "hyprland-git is already installed..."
+	else
+		sudo -u $USER yay -S hyprland-git
+	fi
 
-  install_packages sddm dunst pipewire wireplumber xdg-desktop-portal-hyprland polkit-kde-agent qt5-wayland qt6-wayland
+	install_packages sddm dunst pipewire wireplumber xdg-desktop-portal-hyprland polkit-kde-agent qt5-wayland qt6-wayland
 
 	echo "Installing eww..."
 	# eww
@@ -186,15 +184,14 @@ install_hyprland() {
 }
 
 install_packages() {
-  while [ $# -gt 0 ]; do
-    if pacman -Qs $1 >/dev/null; then
-      echo "$1 is already installed..."
-    else
-      pacman -S $1
-    fi
-    shift
-    ;;
-  done
+	while [ $# -gt 0 ]; do
+		if pacman -Qs $1 >/dev/null; then
+			echo "$1 is already installed..."
+		else
+			pacman -S $1
+		fi
+		shift
+	done
 }
 
 # Check and install selected components
