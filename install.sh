0225b74eea0ba190f6b83ae13fee555ffb7fcca2
langgeorg61@gmail.com <langgeorg61@gmail.com>
2023-09-09 18:49:13 +0200

    .

--- install.sh
+++ install.sh
@@ -194,7 +194,7 @@ install_hyprland() {
 
 install_rust() {
 	echo "Installing rust..."
-	if ! type "$rustup -V" >/dev/null; then
+	if type "$rustup -V" >/dev/null; then
 		rustup update
 	else
 		curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
