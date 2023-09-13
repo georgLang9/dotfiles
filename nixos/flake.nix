641900c03c1b6cd773738f71ccc6a0b12f85631f
Bonesaw <langgeorg61@gmail.com>
2023-09-13 14:07:07 +0200

    Fixed syntax error

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -17,7 +17,7 @@
           /etc/nixos/configuration.nix
         ];
 	
-	home-manager.nixosModules.home-manager
+	home-manager.nixosModules.home-manager =
 	{
 		home-manager.useGlobalPkgs = true;
 		home-manager.useUserPackages = true;
